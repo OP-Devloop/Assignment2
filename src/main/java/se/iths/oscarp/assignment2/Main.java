52b4867a71c47e226171626178b9da1b7ddde62b
OP-Devloop <oscar.pettersson@iths.se>
2025-09-29 14:05:55 +0200

    Commit

--- src/main/java/se/iths/oscarp/assignment2/Main.java
+++ src/main/java/se/iths/oscarp/assignment2/Main.java
@@ -4,11 +4,7 @@ import se.iths.oscarp.assignment2.model.Application;
 import se.iths.oscarp.assignment2.model.ProductDao;
 import se.iths.oscarp.assignment2.model.UI;
 
-import java.util.Scanner;
-
 public class Main {
-    public static Scanner sc = new Scanner(System.in);
-
     public static void main(String[] args) {
         ProductDao proDao = new ProductDao();
         UI ui = new UI();
